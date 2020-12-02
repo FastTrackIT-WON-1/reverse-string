3f0d9ae504d9bd4821883da255f697d863c43c9d
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-12-02 21:24:26 +0200

    Initial commit

---
+++ reverse-string/reverse-string/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a text=");
+            string text = Console.ReadLine();
+            Console.WriteLine(ReverseString(text));
+        }
+
+        static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            int length = input.Length;
+            StringBuilder reversed = new StringBuilder(length);
+
+            int index = length - 1;
+
+            while (index >= 0)
+            {
+                reversed.Append($"{input[index]}");
+                index--;
+            }
+
+            return reversed.ToString();
+        }
+    }
+}
